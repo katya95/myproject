ebef42d40def9229cd4c1bc1dda054db2e821f56
katya95 <73447171+katya95@users.noreply.github.com>
2020-12-23 03:54:45 +0200

    Add files via upload

---
+++ myproject_23_12/application/views/post_view.php
@@ -0,0 +1,29 @@
+﻿<h1>Посты</h1>
+<p>
+<table width='80%'><tr><td>
+
+Вывод постов
+</td></tr>
+<?php 
+//output posts
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(E_ALL);
+$db = new DB();
+$post=new Model_post($db);
+
+        foreach ($post->get_data() as $item) {
+        echo '<tr><td><b>Тема поста:'.' '. $item['title'].'</b><br/>';
+        echo '<p>'. $item['text'].'</p></td></tr>';
+      }
+echo '</table>';
+	$post->get_title(3);
+	$post->get_text(3);
+	echo $post->get_title(3);
+	echo $post->get_text(3);
+	
+	
+?>
+
+
+
