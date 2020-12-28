a80a26977bf1b5c24fdb16ceb54de78238373924
katya95 <73447171+katya95@users.noreply.github.com>
2020-12-28 12:05:47 +0200

    Delete enter_view.php

--- myproject_23_12/application/views/enter_view.php
+++
@@ -1,8 +0,0 @@
-<h1>¬ход</h1>
- <form action="enter2" method='POST' enctype="multipart/form-data">
-            <p>Name:<br> 
-            <input name='name' type='text'  value='$name' /></p>
-            <p>Password: <br> 
-            <input name='password' type='password'  value='$password' /></p>
-            <input type='submit' value='¬ход'>
-  </form>
